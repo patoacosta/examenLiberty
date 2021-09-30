c0e5834b9102d907a6f50848ea247f4ba5f23eb2
joss.acosta@gmail.com <acosta.90>
2021-09-30 09:58:28 -0500

    Creacion Repositorio

---
+++ src/com/examen/EjercicioA.java
@@ -0,0 +1,54 @@
+package com.examen;
+
+import java.util.Scanner;
+
+public class EjercicioA{
+		
+	public EjercicioA() {
+
+		boolean contiene = false;			
+		int sumaDigitos = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Ingresa un numero entero positivo:");
+		int numeroRecibido = scanner.nextInt();
+		
+		char[] digitos = String.valueOf(numeroRecibido).toCharArray();
+	
+		contiene = buscarDigito(digitos);
+		sumaDigitos = sumar(digitos);
+		
+		if (numeroRecibido % 7 == 0 || contiene == true || sumaDigitos == 7) {
+			System.out.println("pi");
+		}else {
+			System.out.println(numeroRecibido);
+		}
+		
+		System.out.println("\nAplicacion finalizada...");
+		scanner.close();
+	}
+
+	
+		public boolean buscarDigito(char[] digitos) {
+			boolean contiene = false;
+			for(int i = 0; i < digitos.length; i++) {
+				if(digitos[i] == '7') {
+					contiene = true;
+				}
+			}
+			return contiene;
+		}
+		
+		
+		public int sumar(char[] digitos){
+			int suma = 0;
+			for (int i = 0; i < digitos.length; i++) {
+				suma = suma + Character.getNumericValue(digitos[i]);
+			}
+			return suma;
+		}
+
+}
+
+
---
+++ src/com/examen/Menu.java
@@ -0,0 +1,27 @@
+package com.examen;
+
+import java.util.Scanner;
+
+
+public class Menu{
+
+	public static void main(String[] args) {
+				
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingresa un numero: ");
+		int opcion = scanner.nextInt();
+		
+		switch (opcion) {
+		case 1:
+			EjercicioA ejercicioA = new EjercicioA();
+			break;
+		case 2:
+			System.out.println("Opcion 2");
+		default:
+			break;
+		}
+		
+	}
+
+}
